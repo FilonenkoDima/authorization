e9023fe2b06814048847151e83f488f30510898e
DmytroFilonenko <filonenkodmytrodm@gmail.com>
2025-01-14 13:13:35 +0200

    git init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+/// <reference types="@angular/localize" />
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+import { bootstrapApplication } from '@angular/platform-browser';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
