02355d71e286c1289928fbd1e5107a51a5fdb3b0
DmytroFilonenko <filonenkodmytrodm@gmail.com>
2025-01-28 14:18:58 +0200

    separated values to constants

--- src/app/features/dashboard/user-assessments-view/graph-view/graph/graph.component.ts
+++ src/app/features/dashboard/user-assessments-view/graph-view/graph/graph.component.ts
@@ -7,8 +7,8 @@ import {
   UserAssessmentGraphDataModel,
   UserAssessmentGraphModel,
 } from '../../../../../core/models/user-assessment-graph-data.model';
+import { chartOptions, legendsColor } from '../../../../../core/constants/graph.constants';
 
-const legendsColor = ['rgba(255, 87, 51, 0.75)', 'rgb(51, 255, 87, 0.75)', 'rgb(51, 87, 255, 0.75)', 'rgb(255, 51, 168, 0.75)'];
 
 @Component({
   selector: 'app-graph',
@@ -19,35 +19,19 @@ const legendsColor = ['rgba(255, 87, 51, 0.75)', 'rgb(51, 255, 87, 0.75)', 'rgb(
   templateUrl: './graph.component.html',
 })
 export class GraphComponent implements OnInit {
+  protected readonly chartOptions = chartOptions;
+
   graph = input.required<UserAssessmentGraphModel>();
 
   graphData!: UserAssessmentGraphDataModel;
   chartData!: ChartData;
   chartType!: ChartType;
-  chartOptions = {
-    responsive: true,
-    scales: {
-      x: {
-        beginAtZero: true
-      }
-    },
-    plugins: {
-      legend: {
-        labels: {
-          color: 'rgba(0, 0, 0, 0.7)'
-        }
-      }
-    }
-  };
 
   ngOnInit() {
-    const resolvedData = this.graph();
-    this.graphData = resolvedData.data;
-    this.chartType = resolvedData.type as ChartType;
+    this.graphData = this.graph().data;
+    this.chartType = this.graph().type as ChartType;
 
-    if (this.graphData) {
       this.initChart();
-    }
   }
 
   private initChart() {
